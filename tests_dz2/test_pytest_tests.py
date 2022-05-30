508b3440a61eb920bf1bdce010aa32ec65d7de4a
LaiTkiLLa <84670285+LaiTkiLLa@users.noreply.github.com>
2022-05-30 08:28:55 +0300

    Update test_pytest_tests.py

--- tests_dz2/test_pytest_tests.py
+++ tests_dz2/test_pytest_tests.py
@@ -3,8 +3,8 @@ from dz2 import get_info, new_folder,delete_folder
 
 
 def test_new_folder():
-    assert get_info(ya_token= 'AQAAAAAgsrE-AADLW8se8WiLSEWUhO9H3-tfWZM', folder_name='test_3') == 'Папки не существует'
-    assert new_folder(ya_token='AQAAAAAgsrE-AADLW8se8WiLSEWUhO9H3-tfWZM', folder_name='test_3') == 201
-    assert get_info(ya_token='AQAAAAAgsrE-AADLW8se8WiLSEWUhO9H3-tfWZM', folder_name='test_3') == 200
-    assert delete_folder(ya_token='AQAAAAAgsrE-AADLW8se8WiLSEWUhO9H3-tfWZM', folder_name='test_3') == 204
+    assert get_info(ya_token= '', folder_name='test_3') == 'Папки не существует'
+    assert new_folder(ya_token='', folder_name='test_3') == 201
+    assert get_info(ya_token='', folder_name='test_3') == 200
+    assert delete_folder(ya_token='', folder_name='test_3') == 204
 
